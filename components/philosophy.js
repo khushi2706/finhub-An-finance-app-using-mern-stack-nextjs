b47bf8fdebbd19b08fbc4c9fb34a8795de2c5b56
Khushi Patel <khushi27@KHUSHIPATELsMBP.lan>
2024-10-14 22:12:40 +0530

    first commit

---
+++ components/philosophy.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Philosophy() {
+    return (
+        <>
+            <div className='bg-green-200 p-16'>
+                <h1 className='text-center uppercase'>our philosophy</h1>
+                <p>
+                    We aim to create a meaningful impact on your life with our holistic financial solution.
+                    Our vision resonates with the idea of simplifying and assuring ethical presence as a
+                    financial guide to uplift financial awareness.
+                </p>
+
+            </div>
+        </>
+    )
+}
+
+export default Philosophy
