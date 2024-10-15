71ca9a8fd362e57285e6ce407eda1fc9022c4810
Khushi Patel <kdpatel@ibaset.com>
2024-10-15 23:49:10 +0530

    logo and image added

--- components/heroSection.js
+++ components/heroSection.js
@@ -18,8 +18,8 @@ function HeroSection() {
                         </a>
                     </div>
                     <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                        {/* <Image src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" width={500}
-                            height={500} /> */}
+                        <Image src="/hero.jpg" alt="mockup" width={500}
+                            height={500} />
                     </div>
                 </div>
             </section>
